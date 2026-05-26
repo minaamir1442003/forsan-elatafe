835736ed2787f47556a9e246b7f3f17ce59335f6
minaamir1442003 <minaamir92021@gmail.com>
2026-05-26 13:59:34 +0300

    more high

--- lib/features/Resort/screen/ResortPage.dart
+++ lib/features/Resort/screen/ResortPage.dart
@@ -82,7 +82,7 @@ class _ResortPageState extends State<ResortPage>
                   const TaskTimelineWidget(),
                   SizedBox(height: 20,),
                   ResortVisionSection(),
-                SizedBox(height: 60.h),
+                SizedBox(height: 75.h),
                 ],
               ),
             ),
